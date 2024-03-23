93610bc2ad1fc59bb3ad4a93536f54adab8a6f4c
vlazo1214 <vincentl1214@yahoo.com>
2024-03-23 13:07:31 -0400

    finished 2 and 3

--- mp2.R
+++ mp2.R
@@ -14,9 +14,23 @@ attach(df)
 
 
 # Shapiro test for normality
-# After running the following line, we see that the p-value < 0.05, therefore
-# showing proving the population's normality:
 shapiro.test(wage_col)
 
 # Kruskal-Wallis H-test
-# kruskal.test(len~factor(dose),data=df)
+kruskal.test(wage_col~factor(edu_col),data=df)
+
+# Chi-square Goodness-of-Fit test
+counts <- c(17, 16, 67)
+probs <- c(.246, .216, .538)
+
+results <- chisq.test(counts, p = probs)
+print(results)
+
+# Chi-square test for Independence
+status <- matrix(c(77, 19634, 833, 18903), nrow = 2, byrow=TRUE)
+
+dimnames(status) <- list(c("Infection","No Infection"), 
+                        c("Vaccine", "Placebo"))
+
+results <- chisq.test(status)
+print(results)
